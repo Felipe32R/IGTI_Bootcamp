db846c0c52d468f0d69eabb8d6ebd5d5187e66ff
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-05-25 00:02:20 -0300

    index

---
+++ modulo2_react/digital-calculator/src/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './containers/calculator'
+import "./index.css"
+
+
+ReactDOM.render(
+    <div className="screen">
+      <Calculator />
+    </div>,
+    document.getElementById('root')
+);

d11defbf62134c166c9e92f481c7789d68224a13
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-07-05 20:05:15 -0300

    main_app

---
+++ modulo4_vue/pokemon-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: hsl(193, 100%, 97%);
+ 
+}
+
+</style>
